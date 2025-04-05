6840b6f18869ff11e6a4b26f5b4382e21e877b9d
marwhals <marjan.mubarok2012@gmail.com>
2025-04-05 16:50:56 +0100

    Add query parameters example

---
+++ Section15QueryParameterswithSpringMVC/src/main/java/spring6restmvc/mappers/CustomerMapper.java
@@ -0,0 +1,15 @@
+package spring6restmvc.mappers;
+
+import spring6restmvc.entities.Customer;
+import spring6restmvc.model.CustomerDTO;
+import org.mapstruct.Mapper;
+
+
+@Mapper
+public interface CustomerMapper {
+
+    Customer customerDtoToCustomer(CustomerDTO dto);
+
+    CustomerDTO customerToCustomerDto(Customer customer);
+
+}
