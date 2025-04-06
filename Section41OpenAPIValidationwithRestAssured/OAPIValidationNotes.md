7bf73ba9476925848f546dde3c642e8fd7c2ac30
marwhals <marjan.mubarok2012@gmail.com>
2025-04-06 19:36:23 +0100

    OpenAPI Testing

---
+++ Section41OpenAPIValidationwithRestAssured/OAPIValidationNotes.md
@@ -0,0 +1,2 @@
+# OpenAPI Validation with RestAssured
+- Use Swagger Request Validator -- see documentation
---
+++ Section41OpenAPIValidationwithRestAssured/src/main/java/spring6restmvc/repositories/CustomerRepository.java
@@ -0,0 +1,12 @@
+package spring6restmvc.repositories;
+
+import spring6restmvc.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+}
